43cec72122206f8c44063e0d2c93740a38d64155
Chris Langhout <c.e.i.langhout@student.tudelft.nl>
2017-06-21 15:54:45 +0200

    add emote per minute query

--- extract_messages.py
+++ extract_messages.py
@@ -2,6 +2,13 @@ import csv
 import os
 import psycopg2
 import argparse
+import urllib2
+import json
+
+emoteJson = json.loads(urllib2.urlopen("https://twitchemotes.com/api_cache/v2/global.json").read())
+emoteString = "(%s)" % ('|'.join(emoteJson['emotes'].keys()))
+
+print emoteString
 
 parser = argparse.ArgumentParser(description='Retrieve the data from the database and store it in a csv file')
 parser.add_argument('-u', '--user', help='The username for the postgress database')
@@ -12,12 +19,24 @@ print(args.user)
 # Name of the CSV file
 filename = 'data/data.csv'
 
+emote_per_minute_perchannel_query = """
+SELECT viewers.rchannel, AVG(viewers.emotePerMinute) as averageEmotePerMinute
+FROM
+ (SELECT channel as rchannel, COUNT(message) as emotePerMinute
+ FROM chat_log
+     WHERE message ~* '{}'
+ GROUP BY date_trunc('minute', to_timestamp(date/1000)), channel) viewers
+GROUP BY viewers.rchannel
+ORDER BY averageEmotePerMinute;
+""".format(emoteString)
+
 # The query for the database
 query = "SELECT channel, sender, date, message FROM chat_log"
 
+
 msg_per_minute_per_channel_query = """
-SELECT rchannel, AVG(a.rcount) 
-FROM 
+SELECT rchannel, AVG(a.rcount)
+FROM
   	(SELECT channel as rchannel, COUNT(*) as rcount
     FROM chat_log
     GROUP BY date_trunc('minute', to_timestamp(date/1000)), channel) a
@@ -37,7 +56,7 @@ GROUP BY channel
 """
 distinct_chatters_per_minute_query = """
 SELECT viewers.rchannel, AVG(viewers.distinctsenders)
-FROM 
+FROM
 	(SELECT channel as rchannel, COUNT(DISTINCT sender) as distinctsenders
 	FROM chat_log
 	GROUP BY date_trunc('minute', to_timestamp(date/1000)), channel) viewers
@@ -56,8 +75,8 @@ with open('label_dictionary.txt') as f:
 # Get the database connection
 try:
 	conn = psycopg2.connect("""dbname='twitch'
-	                           user='%s' 
-	                           host='localhost' 
+	                           user='%s'
+	                           host='localhost'
 	                           password='%s'""" % (args.user, args.password))
 except:
 	print("Unable to connect to the database")
@@ -70,23 +89,26 @@ if not exists:
 	writer.writeheader()
 
 cur = conn.cursor()
-cur.execute(query)
+cur.execute(emote_per_minute_perchannel_query)
 
-count = 0
-while True:
-	rows = cur.fetchmany()
-	count += len(rows)
-	print(count)
-	if not rows:
-		break
-	for row in rows:
-		genre = label_dict[row[0]]
-		writer.writerow({
-			'genre' : genre,
-			'channel' : row[0],
-			'sender' : row[1],
-			'date' : row[2],
-			'message' : row[3]
-		})
+print cur.fetchall()
+
+def writeToCsv (queryResult) :
+    count = 0
+    while True:
+    	rows = queryResult.fetchmany()
+    	count += len(rows)
+    	print(count)
+    	if not rows:
+    		break
+    	for row in rows:
+    		genre = label_dict[row[0]]
+    		writer.writerow({
+    			'genre' : genre,
+    			'channel' : row[0],
+    			'sender' : row[1],
+    			'date' : row[2],
+    			'message' : row[3]
+    		})
 
 f.close()
