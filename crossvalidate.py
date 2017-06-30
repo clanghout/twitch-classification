f2c7cd65953e0013fae0644070ef084636b33cc4
Mark Pasterkamp <m.pasterkamp@student.tudelft.nl>
2017-06-30 15:32:09 +0200

    tested tf-idf via cross-validation

--- crossvalidate.py
+++ crossvalidate.py
@@ -1,5 +1,8 @@
 import csv
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import KFold
+from tfidf_documents import tf_idf_predict
 
 # Load channel dictionary
 label_dict = {}
@@ -12,14 +15,34 @@ with open('label_dictionary.txt') as f:
 
 # Load training data (to test performance on unseen data)
 messages = {}
-with open('data/data.csv') as csvfile:
+with open('data/data_new.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
 		channel = row['channel']
 		message = row['message']
+		if channel not in label_dict:
+			continue
 		if channel not in messages:
 			messages[channel] = []
 		messages[channel].append(message)
 
-data = pd.DataFrame(messages)
-channels = data.channel.unique()
+for k in messages:
+	m = messages[k]
+	messages[k] = '\n'.join(m)
+
+data = pd.DataFrame(list(messages.items()), columns=['channel', 'messages'])
+data['label'] = data['channel'].apply(lambda x: label_dict[x])
+channels = data.channel
+
+kf = KFold(n_splits=4)
+results = []
+for train, test in kf.split(channels):
+	predicted = tf_idf_predict(data[data['channel'].isin(channels[train])],
+							   data[data['channel'].isin(channels[test])])
+	labels = data[data['channel'].isin(channels[test])]['label']
+	
+	results.extend(zip(labels, predicted))
+
+res = pd.DataFrame(results, columns=['label', 'prediction'])
+counts = res.groupby(['label', 'prediction']).size()
+print(counts)
--- tfidf_documents.py
+++ tfidf_documents.py
@@ -1,71 +1,28 @@
-import csv
-import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from nltk.corpus import stopwords
 
-# Load channel dictionary
-label_dict = {}
-with open('label_dictionary.txt') as f:
-	for line in f:
-		dat = line.split()
-		key = dat[0]
-		val = ' '.join(dat[1:])
-		label_dict[key] = val
 
-# Load training data (to test performance on unseen data)
-messages = {}
-with open('data/data.csv') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		channel = row['channel']
-		message = row['message']
-		if channel not in messages:
-			messages[channel] = []
-		messages[channel].append(message)
+def tf_idf_train(data):
+	count_vect = CountVectorizer(stop_words = stopwords.words('english'))
+	
+	# Do the tf-idf part
+	X_train_counts = count_vect.fit_transform(data['messages'])
 
-# All messages are stored in a dict(key, List[message]), so we concatenate the message by a newline
-# so we have dict(key, message)
-count_vect = CountVectorizer()
-for k in messages.keys():
-	message_list = messages[k]
-	message = '\n'.join(message_list)
-	messages[k] = message
+	tfidf_transformer = TfidfTransformer()
+	X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
-# Do the tf-idf part
-X_train_counts = count_vect.fit_transform(messages.values())
+	# Train naive bayes classifier
+	clf = MultinomialNB().fit(X_train_tfidf, data['label'])
+	
+	return (count_vect, tfidf_transformer, clf)
+	
+def tf_idf_predict(train, test):
+	count_vect, tfidf_transformer, clf = tf_idf_train(train)
+	docs_new = test['messages']
+	X_new_counts = count_vect.transform(docs_new)
+	X_new_tfidf = tfidf_transformer.transform(X_new_counts)
 
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-
-# Train naive bayes classifier
-clf = MultinomialNB().fit(X_train_tfidf, list(messages.keys()))
-
-# Load test data
-new_messages = {}
-with open('data/data_chak.csv') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		channel = row['channel']
-		message = row['message']
-		if channel not in new_messages:
-			new_messages[channel] = []
-		new_messages[channel].append(message)
-
-docs_new = []
-for k in new_messages.keys():
-	message_list = new_messages[k]
-	message = '\n'.join(message_list)
-	docs_new.append(message)
-
-# Predict outcome
-X_new_counts = count_vect.transform(docs_new)
-X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-
-predicted = clf.predict(X_new_tfidf)
-
-# Print results
-for i in range(0, len(predicted)):
-	print(predicted[i], ' ', label_dict[predicted[i]], ' ', list(new_messages.keys())[i])
-
+	predicted = clf.predict(X_new_tfidf)
+	return predicted
